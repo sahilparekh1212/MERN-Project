77171182920174978c20db76d571f80e91c433f7
= <sahiparekh1212@gmail.com>
2023-01-19 23:32:30 -0500

    Updated Comment for API= restaurants?name=

--- backend/dao/RestaurantsDAO.js
+++ backend/dao/RestaurantsDAO.js
@@ -20,7 +20,7 @@ export default class RestaurantsDAO {
         let query;
         if (filters) {
             if ("name" in filters) {
-                //DB should have an index with name as "name"
+                //DB should have an index with name as "name" for it to work
                 query = { $text: { $search: filters["name"] } }
             } else if ("cuisine" in filters) {
                 query = { "cuisine": { $eq: filters["cuisine"] } }
